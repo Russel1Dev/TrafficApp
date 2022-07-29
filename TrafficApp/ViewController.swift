dd9178c3aebd7cd89df296016d548a95283c617b
Ruslan Nabiullin <whitebarsfan@gmail.com>
2022-07-29 07:25:57 +0300

    Initial Commit

---
+++ TrafficApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficApp
+//
+//  Created by Mac on 29/07/2022.
+//  Copyright © 2022 Russel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
